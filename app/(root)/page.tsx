4313ae1a64e0cfeb1afccb9b1238632af3138d7a
federicodonati07 <realeaquila.929@gmail.com>
2024-08-10 02:42:14 +0200

    prende valori nome, cognome, password, cpassword, email. Manca prendere data e sezione e logica di registrazione tramite supabase email password.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col'>
+        <div className='title flex justify-center items-center tracking-widest font-bold m-2'>
+            <span className='text-4xl'>TalStatus</span>
+        </div>
+    </div>
+  )
+}
+
+export default page
